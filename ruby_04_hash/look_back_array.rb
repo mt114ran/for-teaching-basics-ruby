1ee0747c6fcb099fed37ab18fee7c3bf6c48b205
maitatomoya <mt114r.an@gmail.com>
2023-06-10 01:17:16 +0900

    add dir hash

---
+++ ruby_04_hash/look_back_array.rb
@@ -0,0 +1,21 @@
+# ====================================
+# 前回のレッスン「配列」の宿題
+# ====================================
+# 1. touchコマンドでファイル：restaurants_searcher.rbを作ってみてください
+# 2. 最終課題：step2のコードを手打ちで入力して書き移してみましょう
+
+# ===ここからは5行目以降に自分でコードをかいていく===
+# 3. 変数responce に入っている文字列を「取得したデータ」だと思って、配列restaurantsに格納してみてください
+# 4. putsをつかって配列restaurantsに格納した値を表示する処理を書いてみてください
+
+# === ここでrestaurants_searcher.rb を保存してください===
+# 5, 最後にターミナルで ruby restaurants_searcher.rb を実行してみましょう。
+# hugahuga が表示されればOKです。
+
+numlist = ["one", "two", "three"]
+puts numlist[1]
+
+# stringlist = []
+# stringlist.append("samurai")
+# puts stringlist
+
---
+++ ruby_04_hash/restaurants_sercher.rb
@@ -0,0 +1,10 @@
+# restaurants_searcher.rb
+
+restaurants = []
+response = "hugahuga"   #取得したデータ
+
+# 空配列restaurantsに取得したデータresponseを格納する
+restaurants.append(response);
+
+# putsを使ってrestaurantsに格納した値を表示する
+puts restaurants
