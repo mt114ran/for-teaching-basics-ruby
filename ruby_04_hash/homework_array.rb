782c85b8e52e80aa19ac92f915069ae3557aa043
maitatomoya <mt114r.an@gmail.com>
2023-06-10 08:32:08 +0900

    update hash

---
+++ ruby_04_hash/homework_array.rb
@@ -0,0 +1,17 @@
+# ====================================
+# 前回のレッスン「配列」の宿題
+# ====================================
+# 1. touchコマンドでファイルrestaurants_searcher.rbを作成する
+# 2. 「最終課題：step2」のコードを手打ちで入力して書き移す
+# 3. 変数responce に入っている文字列を
+#   「取得したデータ」だと思って配列restaurantsに格納する
+# 4. putsをつかって配列restaurantsに格納した値を表示する処理を書く
+# 5, 最後にターミナルで ruby restaurants_searcher.rb を実行する
+# ✔︎  hugahuga が表示されればOKです。
+
+restaurants = []
+response = "hugahuga"   #取得したデータ
+
+restaurants.append(response);
+
+puts restaurants
